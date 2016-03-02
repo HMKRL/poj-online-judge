c488131267dd8017cabe2b01fb0226145cc9e956
Yu-Cheng Liang <a135246a135246@gmail.com>
2016-03-02 14:22:43 +0800

    Finished 3067

--- 3067.cpp
+++ 3067.cpp
@@ -5,31 +5,58 @@ using namespace std;
 struct Road {
 	int east_city;
 	int west_city;
-};
+} road[1000000], sorted[1000000];
+
+long long int cnt;
+
+void merge(int low, int high, int mid)
+{
+	int l = low, h = mid + 1, s = 0;
+	while(l <= mid || h <= high) {
+		if(l > mid) sorted[s++] = road[h++];
+		else if(h > high) sorted[s++] = road[l++];
+		else if(road[h].east_city < road[l].east_city) {
+			sorted[s++] = road[h++];
+			cnt += mid - l + 1;
+		}
+		else sorted[s++] = road[l++];
+	}
+	s = 0;
+	for(l = low;l <= high;++l) {
+		road[l] = sorted[s++];
+	}
+	return;
+}
+
+void mergesort(int low, int high)
+{
+	int mid = (low + high) / 2;
+	if(high > low) {
+		mergesort(low, mid);
+		mergesort(mid + 1, high);
+		merge(low, high, mid);
+	}
+	return;
+}
 
 bool cmp(Road a, Road b)
 {
-	if(a.east_city != b.east_city) return a.east_city < b.east_city;
-	else return a.west_city < b.west_city;
+	if(a.west_city != b.west_city) return a.west_city < b.west_city;
+	else return a.east_city < b.east_city;
 }
 
 int main()
 {
-	int tests, count = 0, N, M, K, i, j, cross = 0;
-	Road road[1000000];
-	scanf("%d", &tests);
+	long long int tests, count = 0, N, M, K, i;
+	scanf("%lld", &tests);
 	while(count++ < tests) {
-		scanf("%d %d %d", &N, &M, &K);
-		for(j = 0;j < K;++j) {
-			scanf("%d %d", &road[j].east_city, &road[j].west_city);
+		scanf("%lld %lld %lld", &N, &M, &K);
+		for(i = 0;i < K;++i) {
+			scanf("%d %d", &road[i].west_city, &road[i].east_city);
 		}
 		sort(road, road + K, cmp);
-		for(i = 0;i < K;++i) {
-			for(j = i;j < K;++j) {
-				if(road[i].east_city == road[j].east_city) continue;
-				if(road[j].west_city < road[i].west_city) ++cross;
-			}
-		}
-		printf("Test case %d: %d\n", count, cross);
+		cnt = 0;
+		mergesort(0, K - 1);
+		printf("Test case %lld: %lld\n", count, cnt);
 	}
 }
