eb51b9981bc5662da432373455a8bf5442f4f11a
Yu-Cheng Liang <a135246a135246@gmail.com>
2016-05-11 13:44:23 +0800

    Finished 3259

--- 3259.cpp
+++ 3259.cpp
@@ -1,6 +1,7 @@
 #include<cstdio>
 #include<utility>
 #include<vector>
+#define INF 999999999
 using namespace std;
 
 int dis[2501][2501];
@@ -21,7 +22,7 @@ int main()
 			for(int j = 0;j <= N + 1;++j) {
 				dis[i][j] = 0;
 			}
-			PATH[i] = 2147483647;
+			PATH[i] = INF;
 		}
 		PATH[N] = 0;
 		for(int i = 0;i < M;++i) {
@@ -34,7 +35,7 @@ int main()
 		}
 		for(int i = 0;i < W;++i) {
 			scanf("%d %d %d", &a, &b, &DIS);
-			if(dis[a][b]) dis[a][b] = dis[a][b] < DIS ? dis[a][b] : DIS;
+			if(dis[a][b]) dis[a][b] = dis[a][b] < -DIS ? dis[a][b] : -DIS;
 			else dis[a][b] = -DIS;
 			V.push_back(make_pair(a, b));
 		}
@@ -42,7 +43,7 @@ int main()
 			for(Vit = V.begin();Vit != V.end();++Vit) {
 				j = Vit -> first;
 				k = Vit -> second;
-				if(PATH[j] != 2147483647) {
+				if(PATH[j] != INF) {
 					PATH[k] = PATH[k] < PATH[j] + dis[j][k] ? PATH[k] : PATH[j] + dis[j][k];
 				}
 			}
@@ -51,14 +52,14 @@ int main()
 		for(Vit = V.begin();Vit != V.end();++Vit) {
 			j = Vit -> first;
 			k = Vit -> second;
-			if(PATH[j] != 2147483647) {
-				if(PATH[k] < PATH[j] + dis[j][k]) {
+			if(PATH[j] != INF) {
+				if(PATH[k] > PATH[j] + dis[j][k]) {
 					test = true;
 					break;
 				}
 			}
 		}
-		if(!test) puts("YES");
+		if(test) puts("YES");
 		else puts("NO");
 	}
 }
